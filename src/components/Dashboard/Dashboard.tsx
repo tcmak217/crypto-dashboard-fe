a7628480d3b0002518ac14f94fcf5eb3e497b46e
tcmak217 <tcmak217@outlook.com>
2023-02-17 14:52:40 +0800

    feat: add quote and dashboard components

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Quote from "../Quote/Quote"
+
+function Dashboard() {
+  return (
+    <div>
+      <Quote></Quote>
+      <Quote></Quote>
+      <Quote></Quote>
+      <Quote></Quote>
+      <Quote></Quote>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/components/Quote/Quote.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Quote() {
+  return <div>This is a quote.</div>
+}
+
+export default Quote
