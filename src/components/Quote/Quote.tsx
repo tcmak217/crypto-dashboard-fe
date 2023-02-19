e0deeb042075a86271d0cddf923a0fef56d11994
tcmak217 <tcmak217@outlook.com>
2023-02-20 00:22:40 +0800

    fix: the card width is not fixed
    The card width is varied by the size of its content and adding a min width can fix it.

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -3,7 +3,7 @@ import { IQuote } from "../../types/quotes"
 function Quote({ quote }: { quote: IQuote }) {
   return (
     <>
-      <div className="rounded-lg border-gray-300 p-2 border-2 min-w-[320px] mr-2 my-2">
+      <div className="rounded-lg border-gray-300 p-2 border-2 w-[350px] min-w-[350px] mr-2 my-2">
         <div className="text-4xl font-bold mb-2">{quote.cryptoName}</div>
         <div className="text-xl my-2 font-bold text-orange-400">${quote.price}</div>
         <div className="flex">
