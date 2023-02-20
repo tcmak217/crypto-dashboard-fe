9e1c70f2a514f475475120555eded42c116c7274
tcmak217 <tcmak217@outlook.com>
2023-02-20 16:39:48 +0800

    feat: limit the number display in 5 decimal places

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -5,16 +5,16 @@ function Quote({ quote }: { quote: IQuote }) {
     <>
       <div className="rounded-lg border-gray-300 p-2 border-2 w-[350px] min-w-[350px] mr-2 my-2">
         <div className="text-4xl font-bold mb-2">{quote.cryptoName}</div>
-        <div className="text-xl my-2 font-bold text-orange-400">${quote.price}</div>
+        <div className="text-xl my-2 font-bold text-orange-400">${quote.price.toFixed(5)}</div>
         <div className="flex">
           <div className="flex-col w-1/2">
             <div className="">volume:</div>
-            <div className="text-gray-500">{quote.volume}</div>
+            <div className="text-gray-500">{quote.volume.toFixed(5)}</div>
           </div>
           <div className="flex-col w-1/2">
             <div className="">change:</div>
             <div className={`${quote.change >= 0 ? "text-green-500" : "text-red-500"}`}>
-              {quote.change}
+              {quote.change.toFixed(5)}
             </div>
           </div>
         </div>
